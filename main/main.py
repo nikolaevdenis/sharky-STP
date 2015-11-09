8704f57cf502c8a17f81871ac58665ddcdc69bb2
Denis Nikolaev <nicolaevdenis@gmail.com>
2015-11-10 00:58:11 +0300

    clean up

--- main/main.py
+++ main/main.py
@@ -1,18 +1,18 @@
 """
-Main file handles input and output of the program, also handles main working process
+main file handles input and output of the program, also handles main working process
 """
 
 # imports
 from classes import Commutator
 from random import randint
 
+# create test commutators list
 test_commutators = []
 for i in range(10):
     test_commutators.append(Commutator(i, i, i))
     for j in range(10):
         test_commutators[i].append(j, j)
 
-for commutator in test_commutators:
-    print (commutator)
-
-print (test_commutators[0].is_connected(5))
+# print ('--DEBUG--')
+# for commutator in test_commutators:
+#     print (commutator)
