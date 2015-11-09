f32186979d34c941f0f747f5a55989a9698ee833
Denis Nikolaev <nicolaevdenis@gmail.com>
2015-11-10 00:18:45 +0300

    Working __init__ and __str__ in Commutator class

--- main/classes.py
+++ main/classes.py
@@ -4,17 +4,18 @@ classes file implies all classes, which will be used in current program
 
 class Commutator:
     def __init__(self, endpoint, port, own_number):
-        # commutator is performed as dictionary in form
-        # endpoint -> port
-        if self.data:
-            self.data.append([endpoint, port])
-        else:
-            self.data = [endpoint, port]
-        self.own_number = own_number
+        # commutator is performed as list in form
+        # [0] - the number of this commutator
+        # [1] - the number of routed commutator
+        # [2] - port leading to routed commutator
+        try:
+            self.data.append([own_number, endpoint, port])
+        except:
+            self.data = [[own_number, endpoint, port]]
     def __str__(self):
         in_string = ''
         for item in self.data:
-            in_string += 'Commutator #' + str(self.own_number) \
-                        + '\troutes to #' + str(item[0])\
-                        + '\tthrough port #' + str(item[1]) + '\n'
+            in_string += 'Commutator #' + str(item[0]) \
+                        + '\troutes to #' + str(item[1])\
+                        + '\tthrough port #' + str(item[2]) + '\n'
         return in_string
--- main/main.py
+++ main/main.py
@@ -2,4 +2,8 @@
 Main file handles input and output of the program, also handles main working process
 """
 
+# imports
+from classes import Commutator
 
+test_commutator = Commutator(2, 80, 1)
+print (test_commutator)
