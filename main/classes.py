24028cca4c85975e7d97b28f1dac85f76e696fcf
denis <nicolaevdenis@gmail.com>
2015-11-10 23:48:24 +0300

    We can now go through the nodes, count path length, destroy backwards connections

--- main/classes.py
+++ main/classes.py
@@ -3,16 +3,23 @@ classes file implies all classes, which will be used in current program
 """
 from random import randint
 
-class Commutators:
+class Network:
 
     def __init__(self, max):
         # creates random network
         self.network = []
         self.root_number = None
-        for i in range(max):
-            self.network.append(Device(0, 0, i))
-            for j in range(max):
-                self.network[i].append(randint(0, j), randint(0, j))
+        # for i in range(max):
+        #     self.network.append(Device(0, 0, i, False))
+        #     for j in range(max):
+        #         self.network[i].append(randint(0, j), randint(0, j))
+        self.network.append(Device(1, 1, 0, False))
+        self.network[0].append(2, 2, False)
+        self.network[0].append(3, 3, False)
+        self.network.append(Device(0, 0, 1, False))
+        self.network.append(Device(0, 0, 2, False))
+        self.network.append(Device(0, 0, 3, False))
+
 
     def __str__(self):
         in_string = ''
@@ -49,17 +56,56 @@ class Commutators:
         # checks if device number is connected to root device
         self.network[device_number].is_connected(self.root_number)
 
+    def dijkstra(self, startpoint, endpoint):
+
+        network = self.network[:]
+
+        start_device = network[startpoint]
+
+        node_list = []
+
+        for device in network:
+            # set incredibly length!
+            node_list.append(10000)
+
+        node_list[startpoint] = 0
+
+        device_number = startpoint
+
+        while device_number != endpoint:
+
+            current_device = network[device_number]
+            current_device_connections = current_device.get_connections()
+            print('current_device', str(device_number), 'current_device_connections', str(current_device_connections))
+            for connection in current_device_connections:
+                if node_list[connection] >= 1 + node_list[device_number]:
+                    node_list[connection] = node_list[device_number] + 1
+
+                # drop connections to current device not to go back
+                # first, get the object of connected device
+                connected_device_port = network[connection].get_port_by_endpoint(device_number)
+                # drop connection to connected device
+                print (connection, connected_device_port)
+                network[connection].drop(connected_device_port)
+                print (str(network[device_number]))
+
+            device_number += 1
+
 class Device:
 
-    def __init__(self, endpoint, port, own_number):
+    def __init__(self, endpoint, port, own_number, root_flag):
         # commutator is performed as list of tuples (endpoint, port used)
-        self.data = [(endpoint, port)]
+        self.data = [[endpoint, port, root_flag]]
         self.number = own_number
         self.is_root = False
 
-    def append(self, endpoint, port):
+    def append(self, endpoint, port, root_flag):
         if all(item[0] != endpoint for item in self.data) and all(item[1] != port for item in self.data):
-            self.data.append((endpoint, port))
+            self.data.append([endpoint, port, root_flag])
+
+    def get_connections(self):
+        # returns all connected nodes as destination node numbers
+        return [connection[0] for connection in self.data]
 
     def __str__(self):
         in_string = 'Commutator #' + str(self.number) + '\n'
@@ -102,4 +148,4 @@ class Device:
     def drop(self, port):
         # closes the port given
         endpoint = self.get_endpoint_by_port(port)
-        self.data.remove((endpoint, port))
+        self.data.remove([endpoint, port, False])
